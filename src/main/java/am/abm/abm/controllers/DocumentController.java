6890966f1e9ca916f4898f1e3cb8dcedc04dc21a
Ashot Achemyan <ashot.atshemyan@gmail.com>
2020-09-26 11:08:44 +0400

    UploadFile

---
+++ src/main/java/am/abm/abm/controllers/DocumentController.java
@@ -0,0 +1,46 @@
+package am.abm.abm.controllers;
+
+import am.abm.abm.models.enities.UploadFileResponse;
+import am.abm.abm.services.DocumentStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+@RestController
+public class DocumentController {
+    private final Path root = Paths.get("src/main/uploads/");
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            return write(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public String write(MultipartFile file) throws IOException {
+        String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyMMddHHmmss-"));
+        String fileName = date + file.getOriginalFilename();
+        String filePath = "src/main/uploads/"  + fileName;
+        Files.copy(file.getInputStream(), Paths.get(filePath), StandardCopyOption.REPLACE_EXISTING);
+        return filePath;
+    }
+}
